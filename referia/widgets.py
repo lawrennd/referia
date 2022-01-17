390ca1fa4830ee7bff08277eaf833e9f5b5f9c81
Neil Lawrence <lawrennd@gmail.com>
2022-01-17 08:09:48 +0000

    Expand capabilities with directory input and regexp column creation. Partially implemented file chooser and default value from preexisting column

--- referia/widgets.py
+++ referia/widgets.py
@@ -1,6 +1,7 @@
 import ipywidgets
 from ipyfilechooser import FileChooser
 import os
+import glob
 
 def MyCheckbox(**args):
     # Deal with weird bug where value is passed as an np.bool_ by wrapping Checkbox
@@ -8,11 +9,22 @@ def MyCheckbox(**args):
     return ipywidgets.Checkbox(**args)
 
 
-def MyFileChooser(**args):    
-    if "path" not in args:
-        path = "."
+def MyFileChooser(**args):
+
+    if "directory" not in args:
+        directory = "."
     else:
-        path = args["path"]
-    root, dirs, files = os.walk(path)
-    args["options"] =[''] + dirs + files
-    return Dropdown(**args)
+        directory = args["directory"]
+        
+    if "glob" not in args:
+        globname = "*"
+    else:
+        globname = args["glob"]
+    files = glob.glob(os.path.join(os.path.expandvars(directory), globname))
+    dirs = []
+    options = []
+    for option in [''] + dirs + files:
+        options.append(os.path.basename(option))
+    args["options"] = options
+    
+    return ipywidgets.Dropdown(**args)
