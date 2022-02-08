9b7e7e87fc9d4c2800678e0285aafa5969a276e6
Neil Lawrence <lawrennd@gmail.com>
2022-02-08 22:41:27 +0000

    Update config to enable inheriting configurations

--- referia/config.py
+++ referia/config.py
@@ -7,49 +7,105 @@ try:
     import gspread_pandas.conf as gspdconf
 except ImportError:
     GSPREAD_AVAILABILE=False
-    
 
-default_file = os.path.join(os.path.dirname(__file__), "defaults.yml")
-local_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "machine.yml"))
-user_file = '_referia.yml'
 
-config = {}
 
-if os.path.exists(default_file):
-    with open(default_file) as file:
-        config.update(yaml.load(file, Loader=yaml.FullLoader))
+def load_user_config(user_file="_referia.yml", directory="."):
+    filename = os.path.join(os.path.expandvars(directory), user_file) 
+    if os.path.exists(filename):
+        with open(filename) as file:
+            conf = yaml.load(file, Loader=yaml.FullLoader)
+        parent = None
+        inherit = {}
+        if "inherit" in conf:
+            inherit = conf["inherit"]
+            del conf["inherit"]
+            parent = load_user_config(user_file, inherit["directory"])
+        if "ignore" not in inherit:
+            inherit["ignore"] = []
+        if "append" not in inherit:
+            inherit["append"] = [] 
+        if parent is not None:
+            additional = []
+            for key, item in parent.items():
+                if key in inherit["ignore"]:
+                    continue
+                if key in inherit["append"] and key in conf:
+                    if key == "additional":
+                        if type(item) is list:
+                            additional = additional + item
+                        else:
+                            additional = additional + [item]
+                        continue
+                    if type(conf[key]) is list:
+                        conf[key] = item + conf[key]
+                        continue
+                    if type(conf[key]) is dict:
+                        item.update(conf[key])
+                        conf[key] = item
+                        continue
+                    else:
+                        raise ValueError("Cannot append to non dictionary or list type.")
+                if key == "scores" and not inherit["writable"]:
+                    additional = additional + [item]
+                    continue
+                if key not in conf:
+                    conf[key] = parent[key]
+                    continue
+                
+            if "additional" in conf:
+                if type(conf["additional"]) is list:
+                    conf["additional"] = additional + conf["additional"]
+                else:
+                    conf["additional"] = additional + [conf["additional"]]
+            elif len(additional)>0:
+                conf["additional"] = additional
+            
+    return conf
 
-if os.path.exists(local_file):
-    with open(local_file) as file:
-        config.update(yaml.load(file, Loader=yaml.FullLoader))
+def load_config():
+    default_file = os.path.join(os.path.dirname(__file__), "defaults.yml")
+    local_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "machine.yml"))
+    user_file = '_referia.yml'
 
-if os.path.exists(user_file):
-    with open(user_file) as file:
-        config.update(yaml.load(file, Loader=yaml.FullLoader))
+    conf = {}
 
-if config=={}:
-    raise ValueError(
-        "No configuration file found at either "
-        + user_file
-        + " or "
-        + local_file
-        + " or "
-        + default_file
-        + "."
-    )
+    if os.path.exists(default_file):
+        with open(default_file) as file:
+            conf.update(yaml.load(file, Loader=yaml.FullLoader))
 
-for key, item in config.items():
-    if item is str:
-        config[key] = os.path.expandvars(item)
+    if os.path.exists(local_file):
+        with open(local_file) as file:
+            conf.update(yaml.load(file, Loader=yaml.FullLoader))
 
-if "logging" in config:
-    if not "level" in config["logging"]:
-        config["logging"]["level"] = 20
-    
-    if not "filename" in config["logging"]:
-        config["logging"]["filename"] = "referia.log"
-else:
-    config["logging"] = {"level": 20, "filename": "referia.log"}
+    conf.update(load_user_config(user_file))
+
+    if conf=={}:
+        raise ValueError(
+            "No configuration file found at either "
+            + user_file
+            + " or "
+            + local_file
+            + " or "
+            + default_file
+            + "."
+        )
+
+    for key, item in conf.items():
+        if item is str:
+            conf[key] = os.path.expandvars(item)
+
+    if "logging" in conf:
+        if not "level" in conf["logging"]:
+            conf["logging"]["level"] = 20
+
+        if not "filename" in conf["logging"]:
+            conf["logging"]["filename"] = "referia.log"
+    else:
+        conf["logging"] = {"level": 20, "filename": "referia.log"}
+    return conf
+
+config = load_config()
 
 conf_dir = None
 file_name = "google_secret.json"
@@ -68,4 +124,3 @@ try:
     )
 except:
     GSPREAD_AVAILABLE=False
-
