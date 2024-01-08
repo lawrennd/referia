f289a8acfb3ce1267ce0264681215be99328c738
Neil Lawrence <lawrennd@gmail.com>
2024-01-08 12:20:31 +0000

    Add back compatible display

---
+++ referia/display.py
@@ -0,0 +1,28 @@
+# Provide compatability with old referia versions by providing referia.data.Data()
+
+from .assess.data import CustomDataFrame
+from .config.interface import Interface
+from .assess.compute import Compute
+from .assess.review import Reviewer
+from . import system
+
+from .util.jupyter import expand_cell
+
+def Scorer(index=None, data=None, user_file="_referia.yml", directory="."):
+    """
+    Return a CustomDataFrame object.
+
+    :param user_file: The name of the user file with interface settings to be loaded in.
+    :type user_file: str
+    :param directory: The directory to look for the user file in.
+    :type directory: str
+    :return: The CustomDataFrame object.
+    """
+
+    interface = Interface.from_file(user_file=user_file, directory=directory)
+    if data is None:
+        data = assess.data.CustomDataFrame.from_interface(interface)
+    if index is None:
+        index = data.index.iloc[0]
+    sys = system.Sys(interface)
+    return Reviewer(index, data, interface, system)
