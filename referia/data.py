a0676dc7a4edd974c4c0f7c53c84572c4432d08b
Neil Lawrence <lawrennd@gmail.com>
2024-01-09 07:00:40 +0000

    Adding in data backward compatability

---
+++ referia/data.py
@@ -0,0 +1,21 @@
+# Provide compatability with old referia versions by providing referia.data.Data()
+
+from .assess.data import CustomDataFrame
+from .config.interface import Interface
+from .assess.compute import Compute
+
+def Data(user_file="_referia.yml", directory="."):
+    """
+    Return a CustomDataFrame object.
+
+    :param user_file: The name of the user file with interface settings to be loaded in.
+    :type user_file: str
+    :param directory: The directory to look for the user file in.
+    :type directory: str
+    :return: The CustomDataFrame object.
+    """
+
+    interface = Interface.from_file(user_file=user_file, directory=directory)
+    data = CustomDataFrame.from_interface(interface)
+    data.compute = Compute(data, interface)
+    return data
